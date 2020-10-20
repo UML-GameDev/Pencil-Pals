ea6dac5fdd3ddc083fb1a1f6219314f0d7f6a835
zedcream <zitcream5@gmail.com>
2020-10-20 00:59:01 -0400

    implemented basic collision detection and left-to-right movement

--- TheCodeBreaker/Assets/Code/Player.cs
+++ TheCodeBreaker/Assets/Code/Player.cs
@@ -5,11 +5,28 @@ using UnityEngine;
 [RequireComponent (typeof(Controller2D))]
 public class Player : MonoBehaviour
 {
+    float moveSpeed = 6;
+    float gravity = -10;
+    Vector3 velocity;
 
     Controller2D controller;
 
     void Start()
     {
         controller = GetComponent<Controller2D>();
+    }
+
+    void Update() {
+
+        if(controller.collisions.top || controller.collisions.bottom)
+        {
+            velocity.y = 0;
+        }
+
+        Vector2 input = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+
+        velocity.x = input.x * moveSpeed;
+        velocity.y += gravity * Time.deltaTime;
+        controller.Move(velocity * Time.deltaTime);
     }
 }
