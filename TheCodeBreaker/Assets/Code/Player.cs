16cc441901e27b26b875732a7dfa0088ec196448
Danny He <zitcream5@gmail.com>
2020-10-23 22:18:48 -0400

    improved movement physics, with configurable ground friction and air resistance

--- TheCodeBreaker/Assets/Code/Player.cs
+++ TheCodeBreaker/Assets/Code/Player.cs
@@ -5,9 +5,12 @@ using UnityEngine;
 [RequireComponent (typeof(Controller2D))]
 public class Player : MonoBehaviour
 {
-    public float moveSpeed = 10;
-    public float jumpVelocity = 20;
-    float gravity = -20;
+    float maxSpeed = 10;
+    float acceleration = 20;
+    float jumpVelocity = 12;
+    float friction = 10;
+    float airFriction = 2;
+    float gravity = -30;
     Vector3 velocity;
 
     Controller2D controller;
@@ -31,7 +34,19 @@ public class Player : MonoBehaviour
             velocity.y += jumpVelocity;
         }
 
-        velocity.x = input.x * moveSpeed;
+        velocity.x += input.x * acceleration * Time.deltaTime;
+        if(Mathf.Abs(velocity.x) >= maxSpeed)
+        {
+            velocity.x = Mathf.Sign(velocity.x) * maxSpeed;
+        }
+
+        if (controller.collisions.bottom && (input.x == 0 || input.x != Mathf.Sign(velocity.x)))
+        {
+            velocity.x -= Mathf.Sign(velocity.x) * Mathf.Abs(velocity.x) * friction * Time.deltaTime;
+        } else if(!controller.collisions.bottom && (input.x == 0 || input.x != Mathf.Sign(velocity.x)))
+        {
+            velocity.x -= Mathf.Sign(velocity.x) * Mathf.Abs(velocity.x) * airFriction * Time.deltaTime;
+        }
 
         velocity.y += gravity * Time.deltaTime;
         controller.Move(velocity * Time.deltaTime);
